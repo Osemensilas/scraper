9f69e839f51388a00e14c434e211d29159b01af6
osemensilas <osemensilas@gmail.com>
2026-04-19 17:58:45 +0100

    update HomeHero.tsx

--- components/sections/HomeHero.tsx
+++ components/sections/HomeHero.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 const HomeHero = () => {
     return ( 
         <>
-        <section className="h-max sm:h-[calc(100vh-50px)] w-screen px-10 flex items-center justify-start relative">
+        <section className="h-max sm:h-[calc(100vh-50px)] w-screen px-10 py-20 sm:py-0 flex items-center justify-start relative">
             <div className="absolute top-0 left-0 h-full w-full">
                 <Image src="/images/hero.jpg" fill className="object-cover" alt="hero image" />
             </div>
             <div className="h-max w-full sm:w-3/5 flex flex-col gap-5 relative z-10">
-                <h1 className="text-5xl font-bold text-foreground">Welcome to Our Web Scraping Service</h1>
-                <p className="text-lg text-foreground/70">Effortlessly extract data from any website with our powerful and user-friendly web scraping solution.</p>
+                <h1 className="text-2xl sm:text-4xl font-bold text-foreground">Welcome to Our Web Scraping Service</h1>
+                <p className="text-bold text-text text-base">Effortlessly extract data from any website with our powerful and user-friendly web scraping solution.</p>
                 <div className="h-max w-max">
-                    <button type="button" className="px-6 py-3 bg-primary text-white rounded-md hover:bg-primary/90 transition">Get Started</button>
+                    <button type="button" className="px-6 py-3 bg-primary text-background rounded-md hover:bg-primary/90 transition">Get Started</button>
                 </div>
             </div>
         </section>
