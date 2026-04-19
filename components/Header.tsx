6656a75ece7331bd994f23e7f991323dac664359
osemensilas <osemensilas@gmail.com>
2026-04-19 18:09:47 +0100

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     const pathname = usePathname();
 
     const [showNav, setShowNav] = useState(false);
-    const [activeUser, setActiveUser] = useState(false);
+    const [activeUser, setActiveUser] = useState(true);
 
     return ( 
         <>
