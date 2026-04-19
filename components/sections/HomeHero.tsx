0a78572b2f832f2835e93b1c3ee65a93ff9f29ac
osemensilas <osemensilas@gmail.com>
2026-04-19 17:35:15 +0100

    update HomeHero.tsx

--- components/sections/HomeHero.tsx
+++ components/sections/HomeHero.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 const HomeHero = () => {
     return ( 
         <>
-        <section className="h-screen w-screen px-10 flex items-center justify-center relative">
-            <div className="h-max w-max flex flex-col gap-5">
+        <section className="h-max sm:h-[calc(100vh-50px)] w-screen px-10 flex items-center justify-start relative">
+            <div className="h-max w-full sm:w-3/5 flex flex-col gap-5">
                 <h1 className="text-5xl font-bold text-foreground">Welcome to Our Web Scraping Service</h1>
                 <p className="text-lg text-foreground/70">Effortlessly extract data from any website with our powerful and user-friendly web scraping solution.</p>
                 <div className="h-max w-max">
                     <button type="button" className="px-6 py-3 bg-primary text-white rounded-md hover:bg-primary/90 transition">Get Started</button>
                 </div>
             </div>
-            <div className="absolute right-10 top-1/2 -translate-y-1/2">
-                <Image src="/home-hero.png" alt="Home Hero Image" width={500} height={500} />
+            <div className="absolute left-0 top-0 h-full w-full">
+                <Image src="/home-hero.png" alt="Home Hero Image" fill className="object-cover" />
             </div>
         </section>
         </>
