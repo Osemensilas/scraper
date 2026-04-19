050d28f5a8da9c67801ab494c95f168d5637b30e
osemensilas <osemensilas@gmail.com>
2026-04-19 17:47:32 +0100

    update

--- components/sections/HomeHero.tsx
+++ components/sections/HomeHero.tsx
@@ -4,7 +4,7 @@ const HomeHero = () => {
     return ( 
         <>
         <section className="h-max sm:h-[calc(100vh-50px)] w-screen px-10 flex items-center justify-start relative">
-            <div className="h-max w-full sm:w-3/5 flex flex-col gap-5">
+            <div className="h-max w-full sm:w-3/5 flex flex-col gap-5 relative z-10">
                 <h1 className="text-5xl font-bold text-foreground">Welcome to Our Web Scraping Service</h1>
                 <p className="text-lg text-foreground/70">Effortlessly extract data from any website with our powerful and user-friendly web scraping solution.</p>
                 <div className="h-max w-max">
